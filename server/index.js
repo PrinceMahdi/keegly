2c5eac056daf376478bb2b68792b3a4e642335ab
Mahdi Shirmohammadi <shirmohammadi2000@gmail.com>
2022-12-15 03:25:20 -0800

    deployed website

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb" }));
 
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, "public")));
 
